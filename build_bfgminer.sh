67ea3079f60fa447a557c9ad9be69108271c8248
Nate Woolls <nwoolls@gmail.com>
2014-05-26 22:55:47 -0400

    Use miner version and executable in cache name to avoid conflicts

--- build_bfgminer.sh
+++ build_bfgminer.sh
@@ -4,5 +4,6 @@
 
 MINER_URL=http://luke.dashjr.org/programs/bitcoin/files/bfgminer/$BFGMINER_VERSION/bfgminer-$BFGMINER_VERSION.zip
 MINER_EXE=bfgminer
+MINER_VERSION=$BFGMINER_VERSION
 
 . ./subs/build_miner.sh
--- build_cgminer.sh
+++ build_cgminer.sh
@@ -4,5 +4,6 @@
 
 MINER_URL=http://ck.kolivas.org/apps/cgminer/cgminer-$CGMINER_VERSION.tar.bz2
 MINER_EXE=cgminer
+MINER_VERSION=$CGMINER_VERSION
 
 . ./subs/build_miner.sh
