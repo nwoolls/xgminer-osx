80489d24cfee59277bde6f1183dec306e59c0f1c
Nate Woolls <nwoolls@gmail.com>
2018-01-13 17:50:43 -0500

    Update BFGMiner to 5.5

--- build_bfgminer.sh
+++ build_bfgminer.sh
@@ -4,7 +4,7 @@ set -e
 
 . ./EDITME_versions.sh
 
-MINER_URL=http://luke.dashjr.org/programs/bitcoin/files/bfgminer/$BFGMINER_VERSION/bfgminer-$BFGMINER_VERSION.zip
+MINER_URL=https://github.com/luke-jr/bfgminer/archive/bfgminer-$BFGMINER_VERSION.tar.gz
 MINER_EXE=bfgminer
 MINER_VERSION=$BFGMINER_VERSION
 
